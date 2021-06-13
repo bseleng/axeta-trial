d967d38a153061666081d51a86549f95ed933786
Bogdan S <bseleng@gmail.com>
2021-06-13 11:59:40 +0300

    feat: add eslint config feat: add input change and click
    Took 50 minutes

--- src/Atoms/AxInput/AxInput.jsx
+++ src/Atoms/AxInput/AxInput.jsx
@@ -1,16 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const AxInput = ({ value, setValue, validation, errorMessage }) => {
   const [showInput, setShowInput] = useState(false)
   const [showDiv, setShowDiv] = useState(true)
+  const inputRef = useRef()
+
+  const toggleVisibility = () => {
+    setShowDiv(!showDiv)
+    setShowInput(!showInput)
+  }
+
+  const handleKeys = (e) => {
+    if (e.charCode === 13) {
+      toggleVisibility()
+    }
+  }
+
   const handleInputChange = (e) => {
     setValue(e.target.value)
   }
 
+  const focusInput = () => {
+    if (showInput) {
+      // inputRef.current.focus()
+      inputRef.current.select()
+    }
+  }
+
+  useEffect(() => {
+    focusInput()
+  }, [showInput])
+
   return (
     <>
-      {showInput && <input value={value} onInput={handleInputChange} />}
-      {showDiv && <div> {value}</div>}
+      {showInput && (
+        <input
+          ref={inputRef}
+          value={value}
+          onInput={handleInputChange}
+          onBlur={toggleVisibility}
+          onKeyPress={handleKeys}
+        />
+      )}
+      {showDiv && <div onClick={toggleVisibility}> {value}</div>}
     </>
   )
 }
