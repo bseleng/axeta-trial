a92d190648cab78550301b144bbce692751af21a
Bogdan S <bseleng@gmail.com>
2021-06-16 00:47:25 +0300

    feat: add change avatar
    Took 20 minutes

--- src/Atoms/AtmAvatar/AtmAvatar.jsx
+++ src/Atoms/AtmAvatar/AtmAvatar.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { RiRefreshFill } from 'react-icons/ri'
 import classes from './AtmAvatar.module.css'
 import defaultPic from '../../Assets/Images/defaultPic.jpg'
 
-const AtmAvatar = ({ avatar = defaultPic, setAvatar }) => {
+const AtmAvatar = ({ avatar = defaultPic }) => {
   const [allowEdit, setAllowEdit] = useState(false)
+  const [file, setFile] = useState(null)
+  const fileRef = useRef()
 
   const handleClick = () => {
     setAllowEdit(true)
   }
+
+  const handleUpload = () => {
+    setFile(fileRef.current.files[0])
+    setAllowEdit(false)
+  }
+
   return (
     <>
       <label htmlFor="image_uploads">
         <input
+          ref={fileRef}
+          onChange={handleUpload}
           type={'file'}
           disabled={!allowEdit}
           accept="image/*"
@@ -20,7 +30,7 @@ const AtmAvatar = ({ avatar = defaultPic, setAvatar }) => {
           id={'image_uploads'}
         />
         <img
-          src={avatar}
+          src={file ? URL.createObjectURL(file) : avatar}
           alt="profile picture"
           onClick={handleClick}
           className={classes.img}
