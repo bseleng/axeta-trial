456200c893468c2e3c40e4ab02f4e2d1b9fa4eb8
Bogdan S <bseleng@gmail.com>
2021-06-16 23:45:29 +0300

    fix: avatar icon on hover
    Took 16 minutes

--- src/Atoms/AtmAvatar/AtmAvatar.jsx
+++ src/Atoms/AtmAvatar/AtmAvatar.jsx
@@ -4,41 +4,38 @@ import classes from './AtmAvatar.module.css'
 import defaultPic from '../../Assets/Images/defaultPic.jpg'
 
 const AtmAvatar = ({ avatar = defaultPic }) => {
-  const [allowEdit, setAllowEdit] = useState(false)
   const [file, setFile] = useState(null)
+  const [showIcon, setShowIcon] = useState(false)
   const fileRef = useRef()
 
-
-  const handleClick = () => {
-      setAllowEdit(true)
-  }
-
   const handleUpload = () => {
-    if (fileRef.current.files && fileRef.current.files[0])
+    if (fileRef.current.files && fileRef.current.files[0]) {
       setFile(fileRef.current.files[0])
-    setAllowEdit(false)
+    }
   }
 
   return (
     <div>
-      <label htmlFor='image_uploads'>
+      <label htmlFor="image_uploads" onClick={handleUpload}>
         <input
           ref={fileRef}
           onChange={handleUpload}
-          onInput={handleClick}
           type={'file'}
-          disabled={!allowEdit}
-          accept='image/*'
+          accept="image/*"
           className={classes.input}
           id={'image_uploads'}
         />
         <img
           src={file ? URL.createObjectURL(file) : avatar}
-          alt='profile picture'
-          onClick={handleClick}
+          alt="profile picture"
+          onMouseOver={() => setShowIcon(true)}
+          onMouseOut={() => setShowIcon(false)}
           className={classes.img}
         />
-        <i className={classes.icon} style={{ visibility: !allowEdit ? 'hidden' : 'visible' }}>
+        <i
+          className={classes.icon}
+          style={{ visibility: showIcon ? 'visible' : 'hidden' }}
+        >
           <RiRefreshFill />
         </i>
       </label>
