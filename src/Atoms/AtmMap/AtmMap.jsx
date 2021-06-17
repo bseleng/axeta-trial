ab058e311b39fef22cca4a4e5e927865a3c106f8
Bogdan S <bseleng@gmail.com>
2021-06-17 22:36:33 +0300

    feat: add google map

--- src/Atoms/AtmMap/AtmMap.jsx
+++ src/Atoms/AtmMap/AtmMap.jsx
@@ -1,14 +1,47 @@
-import React, { useRef, useEffect, useState } from 'react'
-import classes from './AtmMap.module.css'
-const token = process.env.REACT_APP_MAP_API
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+const apiKey = process.env.REACT_APP_MAP_API
 
-const AtmMap = () => {
-
-  return (
-    <div className={classes.mapContainer}>
-      MAP
-    </div>
-  )
+const containerStyle = {
+  width: '552px',
+  height: '200px',
 }
 
-export default AtmMap
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+}
+
+const AtmMap = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: apiKey,
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds()
+    map.fitBounds(bounds)
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={7}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      { /* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(AtmMap)
